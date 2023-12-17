ee7f833de3eb3d7eed1d98d6c01b8787df8e16c1
Robin <robin.pautigny@hetic.net>
2023-12-17 12:09:55 +0100

    Nav bar

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,25 @@ import { Poppins } from 'next/font/google'
 export default function Home() {
   return (
     <main className={styles.main}>
+      <div className={styles.mainNav}>
+        <div className={styles.logo}>
+          <Image
+            src="/logo.svg"
+            alt="Picture of the author"
+            width={100}
+            height={100}
+            ></Image>
+        </div>
+        <div className={styles.nav}>
+          <a href="#">Les avantages</a>
+          <a href="#">À propos</a>
+          <a href="#">Process</a>
+          <a href="#">FAQ</a>
+          <button className={styles.headerCTA}>Réserver un call</button>
+        <div>
+        </div>
+        </div>
+      </div>
       <h1 className={styles.title}>
         Prévisualisez vos posts
       </h1>  
@@ -19,6 +38,7 @@ export default function Home() {
         alt="Picture of the author"
         width={1500}
         height={1500}
+        className={styles.demo}
         ></Image>
     </main>
   )
